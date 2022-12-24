f08eed7261af8f8594139e2e7de33a852e435358
LANNOVA <86397282+LANNOVA@users.noreply.github.com>
2022-12-24 08:50:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,7 @@ label.pack()
 
 
 def restart_program():
-    """Restarts the current program.
-    Note: this function does not return. Any cleanup action (like
-    saving data) must be done before calling this function."""
+    
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
